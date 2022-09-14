63d2c9169f0f2e1a14e3b9ff2ba29a81c041104f
domvito55 <domvito55@gmail.com>
2022-09-13 20:21:30 -0400

    Class2022Sep13

---
+++ Modules/lib.js
@@ -0,0 +1,17 @@
+export default function(){
+    console.log("I did something");
+}
+
+export function halfOf(x){
+    return x/2
+}
+
+export function multiply(x, y){
+    return x*y;
+}
+
+export var flag = false;
+
+export function touch(){
+    flag = true;
+}
---
+++ Modules/main.js
@@ -0,0 +1,13 @@
+// import {halfOf, multiply} from './lib.js';
+// console.log("halfOF: " + halfOf(84));
+// console.log("multiply: " + multiply(84, 2));
+
+// import doSomething from './lib.js';
+// doSomething();
+
+import {flag, touch} from './lib.js';
+console.log(flag);
+touch();
+console.log(flag);
+flag = false;
+console.log(flag);
